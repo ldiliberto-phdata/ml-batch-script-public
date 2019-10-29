572e981b923b3ef4bdfe433f04f784cba4b5c57e
Luke Diliberto <ldiliberto@phdata.io>
2019-10-28 21:26:39 -0500

    add command line args

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # test_repo
 * Test different requirement file formats.
 * Test command line args to python script
+
+# Resources
+https://github.com/awslabs/aws-batch-helpers
--- a_main.py
+++ a_main.py
@@ -1,13 +1,24 @@
+import sys
+
 import nltk
 
-def main():
+def main(repeat_times=3, include_taggging=False, sentence=None):
+    # Arguments come in as strings from main. Must manually coerce.
+    repeat_times = int(repeat_times)
+    include_taggging = True if include_taggging.lower() == 'true' else False
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
-    sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+    if not sentence:
+        sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
     tokens = nltk.word_tokenize(sentence)
-    print(tokens)
-    tagged = nltk.pos_tag(tokens)
-    print(tagged[0:6])
+    for n in range(repeat_times):
+        print(f"repeat time: {n}")
+        print(tokens)
+    if include_taggging:
+        print("Including Tokens")
+        tagged = nltk.pos_tag(tokens)
+        print(tagged[0:6])
 
 if __name__ == "__main__":
-    main()
+    kwargs = {i.split("=")[0]:i.split("=")[1]  for i in sys.argv}
+    main(**kwargs)
