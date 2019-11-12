8f0be527a48411ac376f12491a9548744fb7f337
ldiliberto-phdata <ldiliberto@phdata.io>
2019-11-11 18:11:50 -0600

    Update and rename a_main.py to main_with_reqs.py

--- main_with_reqs.py
+++ main_with_reqs.py
@@ -21,9 +21,14 @@ def main(repeat_times=3, include_tagging=False, sentence=None):
         print(tagged[0:6])
 
 if __name__ == "__main__":
+    # We will get either json list of args, a json map of kwargs, both or neither.
+    args, kwargs = [], {}
     if len(sys.argv) > 1:
-        kwargs = {i.split("=")[0]:i.split("=")[1]  for i in sys.argv[1:]}
-        print(kwargs)
-        main(**kwargs)
-    else:
-        main()
+        argvs = [json.loads(w) for w in sys.argv[1:]]
+        args = [w for w in argvs if isinstance(w, list)]
+        kwargs = [w for w in argvs if isinstance(w, dict)]
+    if len(args) > 1 or len(kwargs) > 1 or len(sys.argv) > 3:
+        raise ValueError("Only one json list (args) and/or one json map (kwargs) allowed.")
+    args = [] if not args else args[0]
+    kwargs = {} if not kwargs else kwargs[0]
+    main(*args, **kwargs)
